0e81651b9c7f8247c0a63e3b1237c7e2cb2489b2
Iron.Snowflake <jadrubio@umich.edu>
2021-04-05 20:51:14 -0400

    Modal for displaying financial definitions from the company profile component

---
+++ src/components/CompanyProfile/DefinitionModal.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal, Button } from '@material-ui/core';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  button: {
+    float: 'right',
+  }
+}));
+
+export default function DefinitionModal({ open, setOpen, info}) {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = useState(getModalStyle);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">{info.displayName}</h2>
+      <p id="simple-modal-description">
+        {info.definition.replace(/\n+/g, '\n').split('\n').map(paragraph => <div className="definition-paragraph">{paragraph}</div>)}
+      </p>
+      <Button className={classes.button} color='primary' variant="contained" onClick={handleClose}>Close</Button>
+    </div>
+  );
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      {body}
+    </Modal>
+  );
+}
