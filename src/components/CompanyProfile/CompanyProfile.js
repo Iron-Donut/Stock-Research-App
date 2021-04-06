df08f85b598dd918a4d9a9780bc445da1891eeab
Iron.Snowflake <jadrubio@umich.edu>
2021-04-05 20:52:21 -0400

    added in modal to display definitions of financial terms

--- src/components/CompanyProfile/CompanyProfile.js
+++ src/components/CompanyProfile/CompanyProfile.js
@@ -1,37 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from "react-redux";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {
+  Table, TableBody, TableCell, TableContainer,
+  TableRow, Paper, TableHead ,
+} from '@material-ui/core';
 import ProfileDisplay from './ProfileDisplayDictionary';
+import DefinitionModal from './DefinitionModal';
+import {
+  dateToMMDDYYYYConverter, floatWithRounding, percentageConverter
+} from '../../utils/stringConversions';
+import './CompanyProfile.css';
 
 function CompanyProfile() {
   const profile = useSelector(state => state.stockInfo.profileInfo);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState({ displayName: "", definition: ``, currency: false });
+
+  const handleModal = (item) => {
+    if (item.definition) {
+      setModalInfo(item)
+      setModalOpen(true);
+    }
+  }
+
+  const displayValue = (item) => {
+    const value = profile[item.name];
+    if(item.currency){
+      return floatWithRounding(value);
+    }
+    if (item.date) {
+      return dateToMMDDYYYYConverter(value);
+    }
+    if (item.percentage) {
+      return percentageConverter(value);
+    }
+    
+    return value;
+  }
 
   const displayRows = () => {
     return (
       ProfileDisplay.map(item => (
-        <TableRow>
-        <TableCell>{item.displayName}</TableCell>
-        <TableCell>{profile[item.name]}</TableCell>
+        <TableRow className="profile-rows" onClick={() => handleModal(item)}>
+          <TableCell>{item.displayName}</TableCell>
+          <TableCell style={{ width: "25%" }}>{displayValue(item)}</TableCell>
         </TableRow>
       )
       )
     )
   }
 
+  const displayHeader = () => {
+    return (
+      <TableHead>
+        <TableRow className="profile-header-row">
+          <TableCell style={{ color: '#ffffff'}}>Snapshot</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableHead>
+    );
+  };
+
   return (
-    <div style={{ width: '25%' }}>
+    <div style={{ width: '20%' }}>
       <TableContainer component={Paper}>
         <Table size="small" aria-label="company-profile">
+          {displayHeader()}
           <TableBody>
             {displayRows()}
           </TableBody>
         </Table>
       </TableContainer>
+      <DefinitionModal open={modalOpen} setOpen={setModalOpen} info={modalInfo}/>
     </div>
   );
 };
